f0f04922c221c85ef28b07262b8314234985f3eb
Karczykowski <115210784+Karczykowski@users.noreply.github.com>
2024-11-06 20:41:48 +0100

    lab5

---
+++ Lab1/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+<meta http-equiv="Content-type" content="text/html; charset=UTF-8" />
+<meta http-equiv="Content-Language" content="pl" />
+<meta name="Author" content="Mariusz Karczykowski" />
+<title>Historia Lotów Kosmicznych</title>
+<link rel="stylesheet" href="css/style.css">
+<script src="js/timedate.js" type="text/javascript"></script>
+<script src="js/kolorujtlo.js" type="text/javascript"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body onload="startclock()">
+	<?php
+		error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING);
+
+		$podstrona = '';
+
+		if (isset($_GET['idp']))
+			$podstrona = $_GET['idp'];
+		else
+			$podstrona = "glowna.html";
+
+		include('html/'.$podstrona)
+?>
+</body>
+</html>
